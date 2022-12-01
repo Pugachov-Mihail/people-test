5582b7bd50d40941341a8ef27033a12f5a3640cd
xd383727@gmail.com <@!lodS9506>
2022-12-01 17:37:44 +0300

    create new serialize and refactors prev models

--- backend/survey/models.py
+++ backend/survey/models.py
@@ -11,12 +11,16 @@ class Profile(models.Model):
                            verbose_name="Возраст")
     date_of_filling_in = models.DateField(null=True, blank=True,
                                           verbose_name="Дата заполнения")
-    student = models.BooleanField(default=False, null=True, blank=True, )
-    university = models.TextField(null=True, blank=True, )
-    gender_info = models.TextField(choices=gender, default="men", null=True, blank=True, )
+    student = models.BooleanField(default=False, null=True, blank=True, verbose_name="Студент")
+    university = models.TextField(null=True, blank=True, verbose_name="Университет")
+    gender_info = models.TextField(choices=gender, default="men", null=True, blank=True, verbose_name="Пол")
 
     def __str__(self):
         return f"{self.gender} {self.age} {self.date_of_filling_in} {self.student} {self.university} {self.gender_info}"
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = "Пользователи"
 
 
 class Survey(models.Model):
@@ -36,15 +40,25 @@ class Survey(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True,
                              verbose_name="Размер порции", choices=CHOISE)
     date_create = models.DateTimeField(auto_created=True)
+    eat = models.ForeignKey("Eat", on_delete=models.PROTECT, null=True, blank=True, verbose_name="Еда")
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = "Опросы"
 
 
 class Eat(models.Model):
     """Еда"""
     title = models.CharField(max_length=50, null=True, blank=True,
                              verbose_name="Наименование продукта")
-    survey_id = models.ForeignKey(Survey, on_delete=models.PROTECT, null=True, blank=True, )
     meal_time = models.DateField(null=True, blank=True, verbose_name="Время приема пищи")
-    drink = models.ForeignKey("Drink", on_delete=models.PROTECT, null=True, blank=True, )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Еда'
+        verbose_name_plural = "Еда"
 
 
 class Work(models.Model):
@@ -53,11 +67,19 @@ class Work(models.Model):
     title = models.CharField(null=True, blank=True, max_length=150)
     other = models.TextField(null=True, blank=True, )
 
+    class Meta:
+        verbose_name = 'Тип работы'
+        verbose_name_plural = "Типы работы"
+
 
 class Emptions(models.Model):
     """Испытываемые эмоцц"""
     user_id = models.ForeignKey(Profile, on_delete=models.PROTECT, null=True, blank=True, )
     title = models.CharField(max_length=70, null=True, blank=True, )
+
+    class Meta:
+        verbose_name = 'Эмоции'
+        verbose_name_plural = "Эмоции"
 
 
 class Drink(models.Model):
@@ -68,3 +90,7 @@ class Drink(models.Model):
     )
     title = models.CharField(max_length=80, null=True, blank=True, choices=CHOISE, default="No")
     other = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Напиток'
+        verbose_name_plural = "Напитки"
--- backend/survey/serializer.py
+++ backend/survey/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, Eat
+from .models import Profile, Eat, Survey, Work, Emptions, Drink
 
 
 class ProfileSerialize(serializers.HyperlinkedModelSerializer):
@@ -11,3 +11,22 @@ class EatSerialize(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Eat
 
+
+class SurveySerialize(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Survey
+
+
+class WorkSerialize(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Work
+
+
+class EmptionsSerialize(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Emptions
+
+
+class DrinkSerialize(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Drink
